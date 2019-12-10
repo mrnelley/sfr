5be30b86c26eb6a8aef6ef94fc23b2067b48be8a
Parnell Kelley <parnelldkelley@gmail.com>
2019-12-10 11:04:12 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+  var cards = $('.card');
+  // returns an array of DOM objects pf class card 
+  for(let i = 0; i < $(cards).length; i++){
+    let $this = $(cards[i]);
+      $this.on('mouseover', function(){
+        $this.toggleClass('hovering');
+      });
+      $this.on('click', function(){
+        $this.toggleClass('clicked');
+      });
+    }
+
+  $('#switch').html("Montgomery County");
+  var state = $('button').html();
+
+  if(state){
+    $('#switch').click(()=>{
+      // change text in button
+      if ($('button').html() == "Montgomery County"){
+        $('button').html("PG County");
+      }
+      else if($('button').html() == "PG County"){
+        $('button').html("Montgomery County");
+      }
+
+      console.log('clicked '+ state);
+      // hide PG info
+      $('#pg_county').toggleClass('hide');
+      // show MoCo info
+      $('#mo_county').toggleClass('hide');
+    })
+  }
+});
+
+
+  // for(var i=0; i < cards.length; i++){
+  //     console.log('clicked card number #' + i );
+  // }
