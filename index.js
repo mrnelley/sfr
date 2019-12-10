606f647c1f9912eb7268b120d729002694b0a629
Parnell Kelley <parnelldkelley@gmail.com>
2019-12-10 11:53:04 -0500

    index

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const port = 3000;
 
 const express = require('express')
+const path = require('path');
 const app = express()
 
-app.get('/', (req, res) => res.send('Jello World!'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname +'/index.html')));
 
 app.listen(process.env.PORT || port, () => {
   console.log(`Server running at http://${port}/`);
