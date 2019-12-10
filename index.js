c4faec12ca093bbe99d69168ab14e1d831b2a7c6
Parnell Kelley <parnelldkelley@gmail.com>
2019-12-10 12:25:47 -0500

    enhanced styling

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const path = require('path');
 const app = express()
 
+app.use(express.static(__dirname + '/public/'));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname +'/index.html')));
 
 app.listen(process.env.PORT || port, () => {
