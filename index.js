fea738283d05cff084dae4cfb6fefe3c2f0d282b
Parnell Kelley <parnelldkelley@gmail.com>
2019-12-10 11:15:53 -0500

    jello world

--- index.js
+++ index.js
@@ -1,39 +1,11 @@
-$(document).ready(function(){
-  var cards = $('.card');
-  // returns an array of DOM objects pf class card 
-  for(let i = 0; i < $(cards).length; i++){
-    let $this = $(cards[i]);
-      $this.on('mouseover', function(){
-        $this.toggleClass('hovering');
-      });
-      $this.on('click', function(){
-        $this.toggleClass('clicked');
-      });
-    }
-
-  $('#switch').html("Montgomery County");
-  var state = $('button').html();
-
-  if(state){
-    $('#switch').click(()=>{
-      // change text in button
-      if ($('button').html() == "Montgomery County"){
-        $('button').html("PG County");
-      }
-      else if($('button').html() == "PG County"){
-        $('button').html("Montgomery County");
-      }
-
-      console.log('clicked '+ state);
-      // hide PG info
-      $('#pg_county').toggleClass('hide');
-      // show MoCo info
-      $('#mo_county').toggleClass('hide');
-    })
-  }
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
 });
-
-
-  // for(var i=0; i < cards.length; i++){
-  //     console.log('clicked card number #' + i );
-  // }
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
